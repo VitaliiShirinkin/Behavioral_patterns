3ba8ae30a798d15f3b744441508350409f52197c
Vitalii Shirinkin <artoiv2@mail.ru>
2023-12-10 18:37:12 +0300

    first commit

---
+++ Chain/Chain.cpp
@@ -0,0 +1,124 @@
+﻿// Chain.cpp : 3. Паттерн «Цепочка ответственности»
+//
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+enum class Type {
+    Warning,
+    Error,
+    FatalError,
+    Unknown
+};
+
+class LogMessage {
+public:
+    LogMessage(Type type, const std::string text):type_(type), text_(text){}
+    Type type()const {
+        return type_;
+        }
+    const std::string& message() const {
+        return text_;
+    }
+private:
+    Type type_;
+    std::string text_;
+};
+
+class LogHandler {
+public:
+    LogHandler(LogHandler* handler = nullptr) : nextHandler(handler) {};
+    virtual ~LogHandler() = default;
+    void setNext(LogHandler* handler) {
+        nextHandler = handler;
+    }
+    virtual void handleMessage(const LogMessage& message) = 0;
+protected:
+    LogHandler* nextHandler;
+};
+
+class FatalError : public LogHandler {
+public:
+    void handleMessage(const LogMessage& message) override {
+        if (message.type() == Type::FatalError) {
+            std::cout << "Fatal Error" << '\n';
+            throw std::runtime_error(message.message());
+        }
+        else {
+            nextHandler->handleMessage(message);
+        }
+    }
+};
+
+class Error : public LogHandler {
+public:
+    Error(const std::string& filename) {
+        out.open(filename);
+        if (!(out.is_open())) {
+            std::cout << "No log file found" << '\n';
+        }
+    }
+    ~Error() {
+        out.close();
+    }
+    void handleMessage(const LogMessage& message) override {
+        if (message.type() == Type::Error) {
+            out << "Error: " << message.message();
+        }
+        else {
+            nextHandler->handleMessage(message);
+        }
+    }
+private:
+    std::ofstream out;
+};
+
+class Warning : public LogHandler {
+public:
+    void handleMessage(const LogMessage& message) override {
+        if (message.type() == Type::Warning) {
+            std::cout << "Warning: " << message.message();
+        }
+        else {
+            nextHandler->handleMessage(message);
+        }
+    }
+};
+
+class Unknown : public LogHandler {
+public:
+    void handleMessage(const LogMessage& message) override {
+        if (message.type() == Type::Unknown) {
+            std::cout << "Unknown: " << message.message();
+        }
+        else {
+            nextHandler->handleMessage(message);
+        }
+    }
+};
+
+int main()
+{
+    LogMessage log1(Type::Error, "This is an error");
+    LogMessage log2(Type::Warning, "This is an warning");
+    LogMessage log3(Type::Unknown, "Unknown message");
+    LogMessage log4(Type::FatalError, "This is fatal error, programm will be closed");
+
+    FatalError fatalErrorHandler;
+    Error errorHandler("out.txt");
+    Warning warningHandler;
+    Unknown unknownHandler;
+
+    fatalErrorHandler.setNext(&errorHandler);
+    errorHandler.setNext(&warningHandler);
+    warningHandler.setNext(&unknownHandler);
+
+    try {
+        fatalErrorHandler.handleMessage(log1);
+        fatalErrorHandler.handleMessage(log2);
+    }
+    catch (std::exception ex) {
+        std::cout << ex.what() << std::endl;
+    }
+}
---
+++ Command/Command.cpp
@@ -0,0 +1,44 @@
+﻿// Command.cpp : Паттерн «Команда»
+//
+#include <fstream>
+#include <iostream>
+
+class LogCommand {
+public:
+    virtual ~LogCommand() = default;
+    virtual void print(const std::string& message) = 0;
+};
+
+class MessageCommand :public LogCommand {
+public:
+    void print(const std::string& message) override {
+        std::cout << message << '\n';
+    }
+};
+
+class FileCommand :public LogCommand {
+public:
+    FileCommand() {
+        in.open("in.txt");
+        if (!(in.is_open())) {
+            std::cout << "No log file found" << '\n';
+        }
+    }
+    ~FileCommand() {
+        in.close();
+    }
+    void print(const std::string& message) override {
+        in << message << '\n';
+    }
+private:
+    std::ofstream in;
+};
+
+int main()
+{
+    MessageCommand text;
+    text.print("message");
+
+    FileCommand log;
+    log.print("message");
+}
